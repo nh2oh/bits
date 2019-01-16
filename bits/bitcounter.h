6710f309db2c92bd97c264ee22687babd423b7f6
ben knowles <ben_junk@ionophore.com>
2019-01-16 15:20:09 -0600

    swap_bits()

--- bits/bitcounter.h
+++ bits/bitcounter.h
@@ -2,6 +2,7 @@
 #include <cstdint>  // CHAR_BIT
 #include <string>
 #include <array>
+#include <iostream>
 
 template<typename T>
 constexpr size_t nbytes(T in) {
@@ -51,7 +52,37 @@ std::array<unsigned char,sizeof(T)> rbyteorder(T in) {
 int tests_rbyteorder();
 
 
-// TODO:  memcpy() ?
+// TODO:  Untested
+template<typename T>
+T swap_bits(T in, size_t i, size_t j) {
+	static_assert(sizeof(T)>0 || (sizeof(T)==0 && i==0 && j==0));
+	//static_assert(i<nbits(in) && j<nbits(in) && i>=0 && j>=0);
+	if (i>nbits(in)-1 || j>nbits(in)-1 || i<0 || j<0) {
+		std::cout << "swap_bits(T in, int8_t i, int8_t j):  "
+			<< "i>nbits(T)-1 || j>nbits(T)-1 || i<0 || j<0.  \n"
+			<< "Aborting...\n";
+		std::abort();
+	}
+
+	//size_t Nchar = nbits(unsigned char c {});
+	size_t Nchar = 8;
+	size_t N = nbits(in);
+	unsigned char *p_in = static_cast<unsigned char*>(static_cast<void*>(&in));
+
+	unsigned char m_i = 1<<(i%Nchar);
+	unsigned char m_j = 1<<(j%Nchar);
+
+	auto bit_i = ((*(p_in+i/Nchar))>>(i%Nchar))&1;
+	auto bit_j = ((*(p_in+j/Nchar))>>(j%Nchar))&1;
+	if (bit_i != bit_j) {
+	//if (((*(p_in+i/Nchar))>>(i%Nchar))&1 != ((*(p_in+j/Nchar))>>(j%Nchar))&1) {
+		*(p_in+i/Nchar) ^= m_i;
+		*(p_in+j/Nchar) ^= m_j;
+	}
+
+	return in;
+};
+
 template<typename T>
 std::array<unsigned char,sizeof(T)> to_byte_array(const T& in) {
 	std::array<unsigned char,sizeof(T)> result {};
