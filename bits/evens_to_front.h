9e0a7a0fe6abe068fc7d3d61a57372e31ec9a5d9
nh2oh <ben@ionophore.com>
2019-01-18 11:52:56 -0600

    dnfp()

---
+++ bits/evens_to_front.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <type_traits>
+
+// Moves all the even numbers in [beg,end) to the front of the range.  Returns an
+// iterator to one past the final even number.  
+template<typename It>
+It evens_to_front(It beg, It end) {
+	static_assert(std::is_integral<std::remove_reference<decltype(*beg)>::type>::value,
+		"Arguments must dereference to yield an integral type.");
+
+	It evens_end = beg;
+	while (beg != end) {
+		if ((*beg)%2 == 0) {
+			std::iter_swap(evens_end,beg);
+			++evens_end;
+		}
+		++beg;
+	}
+
+	return evens_end;
+};
+
+int test_evens_to_front();
+
+
+
+
+template<typename It>
+It dnfp(It beg, It end, It pvt) {
+	auto pvt_v = *pvt;
+	It lt_end = beg;
+	It curr = beg;
+	while (curr != end) {
+		if (*curr < pvt_v) {
+			std::iter_swap(curr,lt_end);
+			++lt_end;
+		}
+		if (*curr == pvt_v) {
+			std::iter_swap(curr,pvt);
+			++pvt;
+		}
+		++curr;
+	}
+
+	return end;
+}
+
+
+
+
+
+
