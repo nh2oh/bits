d8b968b3755fba9e47c2d3d787044007c613160e
ben knowles <ben_junk@ionophore.com>
2019-03-04 11:08:32 -0600

    change_object_type()

--- bits/bitcounter.h
+++ bits/bitcounter.h
@@ -3,6 +3,94 @@
 #include <string>
 #include <array>
 #include <iostream>
+#include <cstring>  // std::memcpy()
+
+//
+// The code for T *change_object_type(U *p) below is taken from:
+// From:  http://www.open-std.org/pipermail/ub/2016-February/000565.html
+// Author:  Richard Smith richardsmith at googlers.com 
+//
+// See also: 
+// From:  http://www.open-std.org/pipermail/ub/2016-February/000561.html
+// Credit:  Hubert Tong hubert.reinterpretcast at gmail.com 
+// Commenting on the following sample from Robert Haberlach (rh633 at cam.ac.uk):
+// 
+//   static_assert(alignof(float)>=alignof(int) && sizeof(float)>=sizeof(int));
+//   int foo(float f) {
+//     return *std::launder(reinterpret_cast<int*>(&f));
+//   }
+//
+// Type-based aliasing analysis (TBAA) is allowed to determine that the 
+// write to "f" and the read from the dereference is not related.  Consider 
+// the effects of inlining:
+// 
+//   int main(void) { return !foo(0.5f); }
+//
+// becomes (loosely):
+//
+//   int main(void) {
+//     int __ret;
+//     {
+//       float __f = 0.5f;
+//       __ret = *std::launder(reinterpret_cast<int *>(&__f));
+//     }
+//     return !__ret;
+//   }
+//
+// The implementation is allowed to observe that there are no TBAA-compatible
+// reads of the value of __f within its lifetime.  The likely result is that
+// the initialization of __f would be optimized away.
+//
+// Robert Haberlach rh633 at cam.ac.uk :
+// Isn't the whole idea that launder's definition is, to some extent, intransparent
+// to optimizers?  We're passing a pointer to f into launder.  As long as the analyser
+// is told to assume that calls to launder perform what not, it cannot elide the 
+// initialization, because launder could read.
+// 
+// Richard Smith richardsmith at googlers.com :
+// launder is known to the optimizer, and is known to not read or write any memory.  
+// If it could, that would significantly inhibit optimizations in code that uses it.  
+//
+//--------------------------------------------------
+// 03/04/19
+// For example: 
+//
+// int make_int_nlegit(double d) {
+//     int *pint = static_cast<int*>(static_cast<void*>(&d));
+//     *pint=5;
+//     return static_cast<int>(d);
+// }          
+//            
+// int main(int argc, char **argv) {
+//     double mydbl {27.31571};
+//     return make_int_nlegit(mydbl);
+// }
+//
+// With Clang x86-64 (Trunk) =>
+// make_int_nlegit(double):                   # @make_int_nlegit(double)
+//         movq    rax, xmm0
+//         movabs  rcx, -4294967296
+//         and     rcx, rax
+//         or      rcx, 5
+//         movq    xmm0, rcx
+//         cvttsd2si       eax, xmm0
+//         ret
+// main:                                   # @main
+//         mov     eax, 27
+//         ret
+//
+template<typename T, typename U>
+T *change_object_type(U *p) {
+	static_assert(sizeof(T) == sizeof(U));
+	static_assert(is_trivially_copyable_v<T> && is_trivially_copyable_v<U>);
+	char buffer[sizeof(T)];
+	std::memcpy(buffer, p, sizeof(T));
+	p->~U();
+	T *result = new (p) T;
+	std::memcpy(result, buffer, sizeof(T));
+	return result;
+}
+
 
 template<typename T>
 constexpr size_t nbytes(T in) {
