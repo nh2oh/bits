e374fbaa051af3eefe4cff4c32fddd7acd1e6db7
ben knowles <ben_junk@ionophore.com>
2019-03-13 10:47:58 -0500

    operator+=()

--- bits/bigint.h
+++ bits/bigint.h
@@ -4,13 +4,42 @@
 #include <vector>
 #include <cstdint>
 
-template<int N> class bigint {
+template<int N>
+class bigint {
 public:
 	bigint();
 	std::string print() const;
 
-	template<typename Tlhs>
+	// LE
+	binint<N>& operator+=(const bigint<N>& rhs) {
+		int c {0};
+		for (int i=0; i<N; ++i) {
+			int s = this->d_[i] + rhs.d_[i] + c;
+			c = s - std::numeric_limits<uint8_t>::max();
+			if (c > 0) {
+				s = std::numeric_limits<uint8_t>::max();
+			} else {
+				c = 0;
+			}
+			this->d_[i] = s;
+		}
+		return this;
+	};
 
+	binint<N>& operator*=(const bigint<N>& rhs) {
+		int c {0};
+		for (int i=0; i<N; ++i) {
+			int s = this->d_[i] + rhs.d_[i] + c;
+			c = s - std::numeric_limits<uint8_t>::max();
+			if (c > 0) {
+				s = std::numeric_limits<uint8_t>::max();
+			} else {
+				c = 0;
+			}
+			this->d_[i] = s;
+		}
+		return this;
+	};
 private:
 	std::array<uint8_t,N> d_ {};
 };
